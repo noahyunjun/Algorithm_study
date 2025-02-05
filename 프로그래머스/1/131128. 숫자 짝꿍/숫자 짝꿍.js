875c9b90dbaaa48257a5d8ef0108437d19df7ea0
NoaH <85290394+noahyunjun@users.noreply.github.com>
2025-02-05 16:43:56 +0900

    [level 1] Title: 숫자 짝꿍, Time: 259.93 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,19 @@
+function solution(X, Y) {
+    let countX = Array(10).fill(0);
+    let countY = Array(10).fill(0);
+
+    for (let char of X) countX[char]++;
+    for (let char of Y) countY[char]++;
+
+    let result = [];
+    for (let i = 9; i >= 0; i--) {
+        let minCount = Math.min(countX[i], countY[i]);
+        result.push(String(i).repeat(minCount));
+    }
+
+    let answer = result.join("");
+    if (answer === "") return "-1";
+    if (answer[0] === "0") return "0";
+
+    return answer;
+}
