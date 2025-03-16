852acfe197a95421ad9f7f11c632c6b5c705571a
NoaH <85290394+noahyunjun@users.noreply.github.com>
2025-03-16 23:54:35 +0900

    [level 0] Title: 배열 만들기 2, Time: 118.39 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,11 @@
+function solution(l, r) {
+    let result = [];
+
+    for (let num = l; num <= r; num++) {
+        if ([...String(num)].every(digit => digit === '0' || digit === '5')) {
+            result.push(num);
+        }
+    }
+
+    return result.length > 0 ? result : [-1];
+}
